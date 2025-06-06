df4ad89c1ddc1229498f9a7592ca3e32175ac834
MahmoudMa2002 <mazenmahmod201@gmail.com>
2025-06-07 02:39:48 +0300

    Update script.js

--- Day05_Blurry_Loading/script.js
+++ Day05_Blurry_Loading/script.js
@@ -1,21 +1,26 @@
 const loadText = document.querySelector('.loading-text');
 const bg = document.querySelector('.bg');
+const progressBar = document.querySelector('.loading-progress');
 
 let load = 0;
+const interval = setInterval(updateProgress, 50);
 
-let int = setInterval(blurring, 30);
-
-function blurring() {
+function updateProgress() {
     load++;
 
-    if (load > 99) {
-        clearInterval(int);
+    if (load > 100) {
+        clearInterval(interval);
+        document.querySelector('.loading-bar').style.opacity = '0';
+        return;
     }
 
-    loadText.innerText = `${load}%`
-    loadText.style.opacity = scale(load, 0, 100, 1, 0)
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+    loadText.textContent = `${load}%`;
+    progressBar.style.width = `${load}%`;
+
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+
+function scale(num, inMin, inMax, outMin, outMax) {
+    return ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 }

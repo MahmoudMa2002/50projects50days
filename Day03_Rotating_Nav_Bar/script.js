539a0f9f090e8083fb803f68327c2f8dcb312666
MahmoudMa2002 <mazenmahmod201@gmail.com>
2025-06-07 01:36:08 +0300

    Update script.js

--- Day03_Rotating_Nav_Bar/script.js
+++ Day03_Rotating_Nav_Bar/script.js
@@ -1,11 +1,13 @@
-const open=document.getElementById('open');
-const close=document.getElementById('close');
-const container=document.querySelector('.container');
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const container = document.querySelector('.container');
 
-open.addEventListener('click', ()=>{
-    container.classList.add('show-nav')
-})
+open.addEventListener('click', () => {
+    container.classList.add('show-nav');
+    document.body.style.overflow = 'hidden'; 
+});
 
-close.addEventListener('click', ()=>{
-    container.classList.remove('show-nav')
-})
+close.addEventListener('click', () => {
+    container.classList.remove('show-nav');
+    document.body.style.overflow = ''; 
+});
